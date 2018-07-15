893d09d5c753dd50e465289efeab13bc33abe763
Navdeep <nav.ddu@gmail.com>
2018-07-15 19:51:11 +0530

    Updated 3 lines
    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,8 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+B <- matrix(c(1,2,3,4),2,2)
+B1 <- makeCacheMatrix(B)
 
 }
 
@@ -11,5 +13,5 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve(B1)        ## Return a matrix that is the inverse of 'x'
 }
